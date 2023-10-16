4030c0f5d4e6c234996a9f9b1fd0ec5ced621484
Bryan <bryan.garcia.cuevas@gmail.com>
2023-10-16 12:27:16 -0700

    getting things ready here

---
+++ main.js
@@ -0,0 +1,13 @@
+//this is going to be the code that organizing all of our functions
+//we will import all of our functions from our ./game folder
+
+//im thinking we have seperate files for player, enemy, battle logic
+
+//import player from './game/player'
+//import enemy from './game/enemy'
+//import battle-logic from './game/battle-logic'
+
+function game() {
+    //will check if there is a save state and load it
+    //game starts and ends here
+}
